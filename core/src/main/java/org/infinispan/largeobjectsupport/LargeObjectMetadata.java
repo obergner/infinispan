6d235fc726a9e4a1a9025c3791e252d413132579
Olaf Bergner <olaf.bergner@gmx.de>
2011-03-31 01:06:21 +0200

    ISPN-78 - Large Object Support
    Implement logic to dissect a Large Object - represented as an
    InputStream - into a collection of chunks of predefined maximum size.
    
     * Implemented LargeObjectMetadata, comprising a LargeObject's key,
    its chunks' keys and its chunks node addresses.
     * Implemented org.infinispan.largeobjectsupport.Chunk, representing a
    LargeObject's chunk.
     * Implemented org.infinispan.largeobjectsupport.Chunks, representing
    a LargObject's entire collection of chunks. This class knows how to
    dissect a LargeObject into single chunks while iterating over it. Plus
    it knows how to create a LargeObject's LargeObjectMetadata.

---
+++ core/src/main/java/org/infinispan/largeobjectsupport/LargeObjectMetadata.java
@@ -0,0 +1,132 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.infinispan.largeobjectsupport;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * <p>
+ * Collects a <em>Large Object</em>'a metadata necessary to store it in INFINISPAN. Since a
+ * <em>Large Object</em> does not fit into a single JVM's heap it needs to be chunked/dissected into
+ * fragments. Each chunk will be stored in a different cluster node under its own chunk key.
+ * <code>LargeObjectMetadata</code> contains the mapping from a <em>Large Object</em>' own key to
+ * its chunk keys, plus additional metadata such as overall size.
+ * </p>
+ * 
+ * @author <a href="mailto:olaf.bergner@gmx.de">Olaf Bergner</a>
+ * @since 5.1
+ */
+public class LargeObjectMetadata<K> implements Serializable, Iterable<String> {
+
+   /** The serialVersionUID */
+   private static final long serialVersionUID = 1100258474820117922L;
+
+   private final K largeObjectKey;
+
+   private final long totalSizeInBytes;
+
+   private final String[] chunkKeys;
+
+   /**
+    * Create a new LargeObjectMetadata.
+    * 
+    * @param largeObjectKey
+    * @param totalSizeInBytes
+    * @param chunkKeys
+    */
+   LargeObjectMetadata(K largeObjectKey, long totalSizeInBytes, String[] chunkKeys) {
+      this.largeObjectKey = largeObjectKey;
+      this.totalSizeInBytes = totalSizeInBytes;
+      this.chunkKeys = chunkKeys;
+   }
+
+   @Override
+   public Iterator<String> iterator() {
+      return Arrays.asList(chunkKeys).iterator();
+   }
+
+   /**
+    * Get the largeObjectKey.
+    * 
+    * @return the largeObjectKey.
+    */
+   public final K getLargeObjectKey() {
+      return largeObjectKey;
+   }
+
+   /**
+    * Get the totalSizeInBytes.
+    * 
+    * @return the totalSizeInBytes.
+    */
+   public final long getTotalSizeInBytes() {
+      return totalSizeInBytes;
+   }
+
+   /**
+    * Get the chunkKeys.
+    * 
+    * @return the chunkKeys.
+    */
+   public final String[] getChunkKeys() {
+      return chunkKeys;
+   }
+
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + Arrays.hashCode(chunkKeys);
+      result = prime * result + ((largeObjectKey == null) ? 0 : largeObjectKey.hashCode());
+      result = prime * result + (int) (totalSizeInBytes ^ (totalSizeInBytes >>> 32));
+      return result;
+   }
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      LargeObjectMetadata other = (LargeObjectMetadata) obj;
+      if (!Arrays.equals(chunkKeys, other.chunkKeys))
+         return false;
+      if (largeObjectKey == null) {
+         if (other.largeObjectKey != null)
+            return false;
+      } else if (!largeObjectKey.equals(other.largeObjectKey))
+         return false;
+      if (totalSizeInBytes != other.totalSizeInBytes)
+         return false;
+      return true;
+   }
+
+   @Override
+   public String toString() {
+      return "LargeObjectMetadata [largeObjectKey=" + largeObjectKey + ", totalSizeInBytes="
+               + totalSizeInBytes + ", chunkKeys=" + Arrays.toString(chunkKeys) + "]";
+   }
+}
