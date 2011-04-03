9de08aa499f7b6499d213bb76aa20bee2cbb18e2
Olaf Bergner <olaf.bergner@gmx.de>
2011-04-03 09:04:35 +0200

    ISPN-78 - Large Object Support
    LargeObjectChunkingInterceptor segments a large object into chunks of
    predefined maximum size and stores each chunk under its own chunk key.
    
     * Make LargeObjectChunkingInterceptor use recently completed class
    Chunks.
     * Make LargeObjectChunkingInterceptor iterate over a large object's
    chunks and call the remainder of the interceptor chain for each chunk,
    replacing the cache entry with the current chunk.
     * Implement a test for LargeObjectChunking interceptor.

--- core/src/main/java/org/infinispan/interceptors/LargeObjectChunkingInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/LargeObjectChunkingInterceptor.java
@@ -22,23 +22,60 @@
 package org.infinispan.interceptors;
 
 import org.infinispan.commands.write.WriteLargeObjectToKeyCommand;
+import org.infinispan.config.Configuration;
+import org.infinispan.container.EntryFactory;
 import org.infinispan.container.entries.CacheEntry;
 import org.infinispan.context.InvocationContext;
+import org.infinispan.distribution.DistributionManager;
+import org.infinispan.factories.annotations.Inject;
 import org.infinispan.interceptors.base.CommandInterceptor;
+import org.infinispan.largeobjectsupport.Chunk;
+import org.infinispan.largeobjectsupport.Chunks;
+import org.infinispan.manager.EmbeddedCacheManager;
 
 import java.io.InputStream;
 
 /**
- * LargeObjectChunkingInterceptor.
+ * <p>
+ * The {@code LargeObjectChunkingInterceptor} is responsible for
+ * <ol>
+ * <li>
+ * dividing a <em>Large Object</em> into {@link org.infinispan.largeobjectsupport.Chunk <code>Chunk
+ * </code>}s, and</li>
+ * <li>
+ * storing each of those {@link org.infinispan.largeobjectsupport.Chunk <code>Chunk</code>}s under
+ * its own <code>chunk key</code>.</li>
+ * </ol>
+ * </p>
  * 
  * @author <a href="mailto:olaf.bergner@gmx.de">Olaf Bergner</a>
  * @see <a href="https://community.jboss.org/wiki/LargeObjectSupport">Large Object Support</a>
  * 
  * @since 5.1
  */
-public class LargeObjectChunkingInterceptor extends CommandInterceptor {
+public class LargeObjectChunkingInterceptor<K> extends CommandInterceptor {
 
-   private final long maxChunkSizeInBytes = 1000000L;
+   // TODO: Make this configurable
+   private long maxChunkSizeInBytes = 1000000L;
+
+   private Configuration configuration;
+
+   private DistributionManager distributionManager;
+
+   private EmbeddedCacheManager embeddedCacheManager;
+
+   private EntryFactory entryFactory;
+
+   @Inject
+   public void init(Configuration configuration, DistributionManager distributionManager,
+            EmbeddedCacheManager embeddedCacheManager, EntryFactory entryFactory,
+            long maxChunkSizeInBytes) {
+      this.configuration = configuration;
+      this.distributionManager = distributionManager;
+      this.embeddedCacheManager = embeddedCacheManager;
+      this.entryFactory = entryFactory;
+      this.maxChunkSizeInBytes = maxChunkSizeInBytes;
+   }
 
    @Override
    public Object visitWriteLargeObjectToKeyCommand(InvocationContext ctx,
@@ -47,10 +84,26 @@ public class LargeObjectChunkingInterceptor extends CommandInterceptor {
          throw new IllegalStateException(
                   "Storing Large Objects in a transactional context is not (yet) supported.");
 
+      CacheEntry rememberedEntry = ctx.lookupEntry(command.getKey());
+      ctx.clearLookedUpEntries();
+
       InputStream largeObject = command.getLargeObject();
-      CacheEntry rememberedCacheEntry = ctx.lookupEntry(command.getKey());
+      Chunks<K> chunks = new Chunks<K>((K) command.getKey(), largeObject, maxChunkSizeInBytes,
+               distributionManager, configuration, embeddedCacheManager);
+      for (Chunk chunk : chunks) {
+         final CacheEntry chunkCacheEntry = entryFactory.wrapEntryForWriting(ctx,
+                  chunk.getChunkKey(), false, false, false, false, false);
+         chunkCacheEntry.setValue(chunk.getData());
+         ctx.putLookedUpEntry(chunk.getChunkKey(), chunkCacheEntry);
+
+         invokeNextInterceptor(ctx, command);
+
+         ctx.clearLookedUpEntries(); // We would otherwise risk getting OOM
+      }
+
+      // Restore former CacheEntry
+      ctx.putLookedUpEntry(command.getKey(), rememberedEntry);
       // We don't need a return value
       return null;
    }
-
 }
