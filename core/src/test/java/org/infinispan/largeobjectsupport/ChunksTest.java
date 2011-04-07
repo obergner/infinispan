5d670a1130ff9f1d8476ddfb0a9daa9a8a6a55ee
Olaf Bergner <olaf.bergner@gmx.de>
2011-04-07 12:24:45 +0200

    ISPN-78 - Large Object Support
    Do not try to ensure that a large object's size does not exceed a
    certain threshold. We do not try to enforce such a limit in case of
    "regular" objects, and it needlessly complicates the code basis.
    
     * Remove code for determining and enforcing a cluster-wide size limit
    from org.infinispan.largeobjectsupport.Chunks. This eliminates Chunks
    dependency on DistributionManager.
     * Eliminate dependency on DistributionManager in
    LargeObjectSupportInterceptor.
     * Simplify tests.

--- core/src/test/java/org/infinispan/largeobjectsupport/ChunksTest.java
+++ core/src/test/java/org/infinispan/largeobjectsupport/ChunksTest.java
@@ -5,15 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import org.infinispan.config.Configuration;
-import org.infinispan.distribution.DistributionManager;
-import org.infinispan.distribution.DistributionManagerImpl;
-import org.infinispan.distribution.TestAddress;
-import org.infinispan.remoting.transport.Address;
 import org.testng.annotations.Test;
 
 /**
@@ -24,17 +18,6 @@ import org.testng.annotations.Test;
  */
 @Test(groups = "unit", testName = "largeobjectsupport.ChunksTest")
 public class ChunksTest {
-
-   private static final int NUM_NODES_IN_CLUSTER = 20;
-
-   private static final Address[] NODE_ADDRESSES;
-
-   static {
-      List<Address> nodeAddresses = new ArrayList<Address>(20);
-      for (int i = 0; i < NUM_NODES_IN_CLUSTER; i++)
-         nodeAddresses.add(new TestAddress(i));
-      NODE_ADDRESSES = nodeAddresses.toArray(new Address[NUM_NODES_IN_CLUSTER]);
-   }
 
    @Test(expectedExceptions = IllegalArgumentException.class)
    public void testThatChunksConstructorRejectsInputStreamNotSupportingMark() {
@@ -49,15 +32,15 @@ public class ChunksTest {
             return false;
          }
       };
-      new Chunks<Object>(new Object(), inputStreamNotSupportingMark, null, null);
+      new Chunks<Object>(new Object(), inputStreamNotSupportingMark, null);
    }
 
    @Test(expectedExceptions = IllegalStateException.class)
    public void testThatChunksInstanceCannotBeIteratedOverMoreThanOnce() {
       InputStream largeObject = new ByteArrayInputStream("This is a large object".getBytes());
       Chunks<Object> objectUnderTest = new Chunks<Object>(new Object(), largeObject,
-               newDistributionManagerWithNumNodesInCluster(NUM_NODES_IN_CLUSTER),
-               newConfigurationWithNumOwnersAndMaxChunkSize(1, 2L));
+
+      newConfigurationWithMaxChunkSize(2L));
 
       for (Chunk chunk : objectUnderTest) {
          chunk.getChunkKey(); // Whatever
@@ -73,8 +56,7 @@ public class ChunksTest {
       long maxChunkSizeInBytes = 3L;
       InputStream largeObject = new ByteArrayInputStream(bytes);
       Chunks<Object> objectUnderTest = new Chunks<Object>(new Object(), largeObject,
-               newDistributionManagerWithNumNodesInCluster(1000),
-               newConfigurationWithNumOwnersAndMaxChunkSize(1, maxChunkSizeInBytes));
+               newConfigurationWithMaxChunkSize(maxChunkSizeInBytes));
       List<Chunk> allChunks = new ArrayList<Chunk>();
 
       for (Chunk chunk : objectUnderTest) {
@@ -90,8 +72,7 @@ public class ChunksTest {
    public void testThatLargeObjectMetadataCannotBeCalledIfIterationNotYetFinished() {
       InputStream largeObject = new ByteArrayInputStream("This is a large object".getBytes());
       Chunks<Object> objectUnderTest = new Chunks<Object>(new Object(), largeObject,
-               newDistributionManagerWithNumNodesInCluster(NUM_NODES_IN_CLUSTER),
-               newConfigurationWithNumOwnersAndMaxChunkSize(1, 2L));
+               newConfigurationWithMaxChunkSize(2L));
       objectUnderTest.iterator().next(); // Should hold more than one chunk
 
       objectUnderTest.largeObjectMetadata();
@@ -104,8 +85,7 @@ public class ChunksTest {
       InputStream largeObject = new ByteArrayInputStream(bytes);
       Object largeObjectKey = new Object();
       Chunks<Object> objectUnderTest = new Chunks<Object>(largeObjectKey, largeObject,
-               newDistributionManagerWithNumNodesInCluster(NUM_NODES_IN_CLUSTER),
-               newConfigurationWithNumOwnersAndMaxChunkSize(1, maxChunkSizeInBytes));
+               newConfigurationWithMaxChunkSize(maxChunkSizeInBytes));
       for (Chunk chunk : objectUnderTest) {
          chunk.getChunkKey(); // Whatever
       }
@@ -122,36 +102,13 @@ public class ChunksTest {
    public void testThatChunkIteratorDoesNotSupportRemove() {
       InputStream largeObject = new ByteArrayInputStream("This is a large object".getBytes());
       Chunks<Object> objectUnderTest = new Chunks<Object>(new Object(), largeObject,
-               newDistributionManagerWithNumNodesInCluster(NUM_NODES_IN_CLUSTER),
-               newConfigurationWithNumOwnersAndMaxChunkSize(1, 2L));
+               newConfigurationWithMaxChunkSize(2L));
 
       objectUnderTest.iterator().remove();
    }
 
-   private DistributionManager newDistributionManagerWithNumNodesInCluster(int numNodesInCluster) {
-      DistributionManager distributionManager = new DistributionManagerImpl() {
-
-         private Random randomGenerator = new Random();
-
-         @Override
-         public List<Address> locate(Object key) {
-            int clusterNodeIndex = randomGenerator.nextInt(NUM_NODES_IN_CLUSTER);
-            return Collections.singletonList(NODE_ADDRESSES[clusterNodeIndex]);
-         }
-
-      };
-
-      return distributionManager;
-   }
-
-   private Configuration newConfigurationWithNumOwnersAndMaxChunkSize(final int numOwners,
-            final long maxChunkSize) {
+   private Configuration newConfigurationWithMaxChunkSize(final long maxChunkSize) {
       return new Configuration() {
-         @Override
-         public int getNumOwners() {
-            return numOwners;
-         }
-
          @Override
          public long getMaximumChunkSizeInBytes() {
             return maxChunkSize;
